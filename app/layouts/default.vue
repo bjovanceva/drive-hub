0d542b18ad5419e9f8001959ba0530cf1c189437
Ljupcho Angelovski <ljupchoangelovski@codechem.com>
2026-08-21 09:19:55 +0200

    refactor: added nuxt structure for frontend

---
+++ app/layouts/default.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import AddHeader from '../components/AddHeader.vue';
+
+
+</script>
+
+<template>
+  <div>
+    <add-header/>
+    <slot />
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
+<template>
+  <div>
+    THis is index
+  </div>
+  <NuxtWelcome />
+</template>
+
+
+<style></style>
